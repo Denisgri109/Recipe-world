e64bc9a2baa108c3a4db5c4165e8d969a28fd796
Roman0Korol <romankorolovich@live.ie>
2026-04-06 23:03:14 +0100

    add CategorySeeder with default recipe categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,19 +16,7 @@ class DatabaseSeeder extends Seeder
         User::factory(5)->create();
 
         // 2. Seed Categories
-        Category::factory()
-            ->count(8)
-            ->sequence(
-                ['name' => 'Breakfast',  'description' => 'Start your day with a hearty meal.'],
-                ['name' => 'Lunch',      'description' => 'Midday meals to keep you going.'],
-                ['name' => 'Dinner',     'description' => 'Delicious evening recipes.'],
-                ['name' => 'Desserts',   'description' => 'Sweet treats and baked goods.'],
-                ['name' => 'Appetizers', 'description' => 'Small bites to get things started.'],
-                ['name' => 'Soups',      'description' => 'Warm and comforting bowls.'],
-                ['name' => 'Salads',     'description' => 'Fresh and healthy options.'],
-                ['name' => 'Snacks',     'description' => 'Quick bites for any time of day.'],
-            )
-            ->create();
+        $this->call(CategorySeeder::class);
 
         // 3. Seed Recipes (with ingredients)
         $this->call(RecipeSeeder::class);
